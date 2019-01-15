39938583008f7e4b9377cb0fbd9e82109cd0dbf4
bonillamiranda3 <bonillamiranda3@gmail.com>
2019-01-15 03:41:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,25 +23,24 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
 "SELECT project.title, SUM(pledges.amount) - projects.funding_goal FROM projects
 JOIN pledges ON pledges.project_id = project.id
-GROUP BY pledges.project_id HAVING SUM(pledges.amount) >= projects.funding_goal;"
-
+GROUP BY pledges.project.title HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
   "SELECT users.name, SUM(pledges.amount) FROM users
   JOIN pledges ON pledges.user_id = users.id
-  GROUP BY users.name
+  GROUP BY name
   ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT project.category, SUM(pledges.amount) FROM projects
 JOIN pledges ON pledges.project_id = projects.id
-WHERE projects.category = 'music'"
+WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
 "SELECT project.category, SUM(pledges.amount) FROM projects
 JOIN pledges ON pledges.project_id = projects.id
-WHERE projects.category = 'books'"
+WHERE projects.category = 'books';"
 end
